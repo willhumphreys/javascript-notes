1626f86010d1148764bf0b388e7bcb1e979d6eaa
Will Humphreys <whumphreys@gmail.com>
2018-10-07 13:33:31 +0200

    Forget to save

--- isPrime.js
+++ isPrime.js
@@ -1,15 +1,20 @@
-let isPrime = function(num) {
-  if (num % 2 === 0) return false;
+const args = process.argv;
+
+let isPrime = num => {
   if (num === 2 || num === 3) return true;
+  if (num % 2 === 0) return false;
 
   let [divisor, limit] = [3, Math.sqrt(num)];
 
   while (divisor <= limit) {
     if (num % divisor === 0) return false;
+
     divisor += 2;
   }
-
   return true;
 };
 
-console.log(isPrime(13));
+const possiblePrime = Number(args[2]);
+
+console.log("Using argument " + possiblePrime);
+console.log(isPrime(possiblePrime));
